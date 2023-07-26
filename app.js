069e044a3c62cc95203da965bae0807c97dc4985
Jimcl6 <jedllorente@gmail.com>
2023-07-27 01:21:21 +0800

    working with arrays

---
+++ app.js
@@ -0,0 +1,12 @@
+let greetingText = 'Hi I\'m Jed' //this is a string
+// when naming variables use the camel casing naming convention.
+// syntax:- let variableName
+let userAge = 32;
+alert(greetingText);
+alert(greetingText);
+greetingText = 'Hi I\'m really Jed';
+alert(greetingText);
+
+
+alert(userAge);
+alert(`I am ${userAge} years old.`);
